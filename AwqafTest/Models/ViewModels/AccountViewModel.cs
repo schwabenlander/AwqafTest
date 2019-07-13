6a78311507f2632436a76aff5d412f849f6adfbc
Sean Schwabenlander <sean@schwabenlander.com>
2019-07-13 12:48:22 +0300

    Created a view model for Accounts

---
+++ AwqafTest/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AwqafTest.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        [Required, Display(Name = "Account ID"), Range(1, int.MaxValue)]
+        public int AccountId { get; set; }
+
+        [Required, Display(Name = "Account Number"), StringLength(100)]
+        public string AccountNumber { get; set; }
+
+        [Required, Display(Name = "Level 1"), Range(0, short.MaxValue)]
+        public short Level1 { get; set; }
+
+        [Required, Display(Name = "Level 2"), Range(0, short.MaxValue)]
+        public short Level2 { get; set; }
+
+        [Required, Display(Name = "Level 3"), Range(0, short.MaxValue)]
+        public short Level3 { get; set; }
+
+        [Required, Display(Name = "Level 4"), Range(0, short.MaxValue)]
+        public short Level4 { get; set; }
+
+        [Display(Name = "User ID"), Range(1, int.MaxValue)]
+        public int? UserId { get; set; }
+
+        [Display(Name = "Remarks"), StringLength(300)]
+        public string Remarks { get; set; }
+    }
+}
