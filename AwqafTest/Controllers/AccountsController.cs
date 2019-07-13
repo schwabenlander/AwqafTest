e60b36d7cbe6a79e73f119ec48f05b187d3f29c0
Sean Schwabenlander <sean@schwabenlander.com>
2019-07-13 13:55:29 +0300

    Implementing Accounts controller and view

--- AwqafTest/Controllers/AccountsController.cs
+++ AwqafTest/Controllers/AccountsController.cs
@@ -7,40 +7,40 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AwqafTest.Database;
 using AwqafTest.Models;
+using AwqafTest.Models.ViewModels;
 
 namespace AwqafTest.Controllers
 {
     public class AccountsController : Controller
     {
-        private readonly AwqafContext _context;
+        private readonly IAccountDataService _accountData;
 
-        public AccountsController(AwqafContext context)
+        public AccountsController(IAccountDataService accountData)
         {
-            _context = context;
+            _accountData = accountData;
         }
 
         // GET: Accounts
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Accounts.ToListAsync());
-        }
+            var accounts = _accountData.GetAccounts();
+            var viewModel = new List<AccountViewModel>();
 
-        // GET: Accounts/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            foreach (var account in accounts)
             {
-                return NotFound();
+                viewModel.Add(new AccountViewModel
+                {
+                    AccountId = account.AccountId,
+                    AccountNumber = account.AccountNumber,
+                    Level1 = account.Level1,
+                    Level2 = account.Level2,
+                    Level3 = account.Level3,
+                    Level4 = account.Level4,
+                    UserId = account.UserId
+                });
             }
 
-            var account = await _context.Accounts
-                .FirstOrDefaultAsync(m => m.AccountId == id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            return View(account);
+            return View(viewModel);
         }
 
         // GET: Accounts/Create
@@ -54,100 +54,9 @@ namespace AwqafTest.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("AccountId,AccountNumber,Level1,Level2,Level3,Level4,SystemDate,Remarks,UserId")] Account account)
+        public IActionResult Create([Bind("AccountId,AccountNumber,Level1,Level2,Level3,Level4,SystemDate,Remarks,UserId")] Account account)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(account);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(account);
-        }
-
-        // GET: Accounts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var account = await _context.Accounts.FindAsync(id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-            return View(account);
-        }
-
-        // POST: Accounts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("AccountId,AccountNumber,Level1,Level2,Level3,Level4,SystemDate,Remarks,UserId")] Account account)
-        {
-            if (id != account.AccountId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(account);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AccountExists(account.AccountId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(account);
-        }
-
-        // GET: Accounts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var account = await _context.Accounts
-                .FirstOrDefaultAsync(m => m.AccountId == id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            return View(account);
-        }
-
-        // POST: Accounts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var account = await _context.Accounts.FindAsync(id);
-            _context.Accounts.Remove(account);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AccountExists(int id)
-        {
-            return _context.Accounts.Any(e => e.AccountId == id);
+            throw new NotImplementedException();
         }
     }
 }
