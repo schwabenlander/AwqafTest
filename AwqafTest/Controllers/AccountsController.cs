af8608307a7ea884fda402e77a9b50035a4a117b
Sean Schwabenlander <sean@schwabenlander.com>
2019-07-15 12:48:19 +0300

    Did some cleanup work

--- AwqafTest/Controllers/AccountsController.cs
+++ AwqafTest/Controllers/AccountsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using AwqafTest.Database;
 using AwqafTest.Models;
 using AwqafTest.Models.ViewModels;
--- AwqafTest/Controllers/FiscalYearsController.cs
+++ AwqafTest/Controllers/FiscalYearsController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AwqafTest.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using AwqafTest.Models;
 using AwqafTest.Models.ViewModels;
 
